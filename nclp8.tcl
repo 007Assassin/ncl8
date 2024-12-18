cf4bb756a7893e469e1c45f2c025c8fd8527b708
007Assassin <assassin.spam7@gmail.com>
2024-12-18 11:23:29 +0530

    Update nclp8.tcl

--- nclp8.tcl
+++ nclp8.tcl
@@ -80,3 +80,32 @@ exit 0
 }
 puts "Starting Simulation..."
 $ns_ run
+
+
+AWK SCRIPT:
+BEGIN {
+recd =0
+hdrsz =0
+stoptime =0
+starttime=0
+}
+{
+time =$2
+if($1=="s" && $4=="AGT" && $8>=512){
+if(time > starttime || starttime ==0){
+starttime = time
+}
+}
+if($1=="r" && $4=="AGT" && $8 >= 512){
+if(time > starttime){
+stoptime = time
+}
+hdrsz =$8%512
+$8-= hdrsz
+recd += $8
+}
+}
+END {
+printf("Gooput = %f Kbps \n",(recd)/(stoptime-starttime)*8/1000)
+}
+
